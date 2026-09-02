830baccad7a88fa45787db4b0b5c532fb5c13a46
eljamaaouy <eljamaaouyayman@gmail.com>
2026-09-02 11:09:30 -0700

    swagger documentation

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +13,10 @@ async function bootstrap() {
   app.enableCors({
     origin: "http://localhost:3000"
   });
+
+  const swagger = new DocumentBuilder().setVersion("1.0").build()
+  const documentation = SwaggerModule.createDocument(app, swagger)
+  SwaggerModule.setup('swagger', app, documentation)
 
   await app.listen(process.env.PORT ?? 3001);
 }
